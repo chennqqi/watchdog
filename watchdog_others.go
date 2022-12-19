9d83a5b25a065a5fc56b97bd4c5680c981979090
Kuba Raczkowski <kubaraczkowski@gmail.com>
2022-12-19 13:50:49 +0100

    device_path for others

--- watchdog_others.go
+++ watchdog_others.go
@@ -13,7 +13,7 @@ import (
 // support this OS.
 var errNotImplemented = fmt.Errorf("watchdog: not implemented on %s: %w", runtime.GOOS, os.ErrNotExist)
 
-func open() (*Device, error)                    { return nil, errNotImplemented }
+func open(device_path string) (*Device, error)  { return nil, errNotImplemented }
 func (*Device) ping() error                     { return errNotImplemented }
 func (*Device) timeout() (time.Duration, error) { return 0, errNotImplemented }
 func (*Device) close() error                    { return errNotImplemented }
